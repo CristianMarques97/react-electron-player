9b0c62d4ec3bd1e98ee858e8d43f8a3867dc6247
= <cristianmarques97@hotmail.com>
2021-07-24 20:26:41 -0300

    Initial Commit

---
+++ src/electron/start.js
@@ -0,0 +1,116 @@
+const { app, BrowserWindow } = require('electron')
+
+const path = require('path')
+const url = require('url')
+
+function handleSquirrelEvent(application) {
+  if (process.argv.length === 1) {
+    return false
+  }
+
+  const ChildProcess = require('child_process')
+
+  const appFolder = path.resolve(process.execPath, '..')
+  const rootAtomFolder = path.resolve(appFolder, '..')
+  const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'))
+  const exeName = path.basename(process.execPath)
+
+  const spawn = (command, args) => {
+    let spawnedProcess
+
+    try {
+      spawnedProcess = ChildProcess.spawn(command, args, {
+        detached: true,
+      })
+    } catch (e) {
+      console.log(e)
+    }
+
+    return spawnedProcess
+  }
+
+  const spawnUpdate = (args) => spawn(updateDotExe, args)
+
+  const squirrelEvent = process.argv[1]
+  switch (squirrelEvent) {
+    case '--squirrel-install':
+    case '--squirrel-updated':
+      // Optionally do things such as:
+      // - Add your .exe to the PATH
+      // - Write to the registry for things like file associations and
+      //   explorer context menus
+
+      // Install desktop and start menu shortcuts
+      spawnUpdate(['--createShortcut', exeName])
+
+      setTimeout(() => application.quit, 1000)
+      return true
+
+    case '--squirrel-uninstall':
+      // Undo anything you did in the --squirrel-install and
+      // --squirrel-updated handlers
+
+      // Remove desktop and start menu shortcuts
+      spawnUpdate(['--removeShortcut', exeName])
+
+      setTimeout(() => application.quit, 1000)
+      return true
+
+    case '--squirrel-obsolete':
+      // This is called on the outgoing version of your app before
+      // we update to the new version - it's the opposite of
+      // --squirrel-updated
+
+      application.quit()
+      return true
+  }
+}
+
+if (handleSquirrelEvent(app)) {
+  // squirrel event handled and app will exit in 1000ms, so don't do anything else
+  return
+}
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  })
+
+  mainWindow.loadURL(
+    process.env.ELECTRON_START_URL
+      || url.format({
+        pathname: path.join(__dirname, '/../../build/index.html'),
+        protocol: 'file:',
+        slashes: true,
+      }),
+  )
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+app.on(
+  'window-all-closed',
+  () => process.platform !== 'darwin' && app.quit(), // "darwin" targets macOS only.
+)
