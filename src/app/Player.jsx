91d239255535f4911bff5675a312503c0817dedf
Cristian <cristianmarques97@hotmail.com>
2021-07-26 14:35:26 -0300

    Linux and windows build scripts

--- src/app/Player.jsx
+++ src/app/Player.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import ReactPlayer from 'react-player'
 
 export default function MusicPlayer() {
   return (
     <>
-      <ReactPlayer url="D:/Projetos/colors-sonic-colors-ultimate-new-remix.mp3" />
     </>
   )
 }
