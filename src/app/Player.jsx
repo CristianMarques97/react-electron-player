9ed8dfc64af07a5e1a5e57c7e6f34e7a97bc2680
= <cristianmarques97@hotmail.com>
2021-07-25 22:09:48 -0300

    Environment fixes

--- src/app/Player.jsx
+++ src/app/Player.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
-import Playlist from 'react-mp3-player'
+import ReactPlayer from 'react-player'
 
 export default function MusicPlayer() {
-  const options = {
-    offset: {
-      left: 300,
-    },
-    breakpoint: {
-      maxWidth: 768,
-    },
-  }
-  const tracks = [{
-    img: 'https://icon-library.net/images/music-icon-transparent/music-icon-transparent-11.jpg', name: 'MP3', desc: 'Description 1', src: 'Audio.mp3',
-  },
-  {
-    img: 'https://icon-library.net/images/music-icon-transparent/music-icon-transparent-11.jpg', name: 'MP3 #2', desc: 'Description 2', src: 'Audio2.mp3',
-  }]
   return (
     <>
-      <Playlist tracks={tracks} opts={options} />
+      <ReactPlayer url="D:/Projetos/colors-sonic-colors-ultimate-new-remix.mp3" />
     </>
   )
 }
